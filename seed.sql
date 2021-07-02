f82bcd29cef52113efcb0f37ff5553c766fc60bd
Emma Mattner <emattner1997@gmail.com>
2021-07-02 13:38:58 +0930

    updating seed

--- seed.sql
+++ seed.sql
@@ -6,7 +6,7 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal'),
-('Security');
+('Human Resources');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
@@ -15,14 +15,14 @@ VALUES
 ('Lead Engineer', 150000, 2),
 ('Accountant', 125000, 3),
 ('Lawyer', 190000, 4),
-('Legal Team Lead', 250000, 4);
+('Manager', 95000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('Homer', 'Simpson', 1, 302),
-('Tommy', 'Pickles', 4, 954),
+('Tommy', 'Pickles', 1, 954),
 ('Travis', 'Scott', 2, 451),
-('Rick', 'Sanchez', 4, 113),
-('Harry', 'Potter', 3, 639),
-('Rosemary', 'Jones', 1, 287);
+('Rick', 'Sanchez', 3, 113),
+('Harry', 'Potter', 4, 639),
+('Rosemary', 'Jones', 5, 287);
 
