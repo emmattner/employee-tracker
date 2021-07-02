767d5558de2f1045847449453108060dd55eeb4b
Emma Mattner <emattner1997@gmail.com>
2021-07-02 13:27:17 +0930

    insert role seed

--- seed.sql
+++ seed.sql
@@ -6,4 +6,16 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal'),
-('Security')
+('Security');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Sales Person', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Accountant', 125000, 3),
+('Lawyer', 190000, 4),
+('Legal Team Lead', 250000, 4);
+
+
+
